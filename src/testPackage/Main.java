1088ac824ecab00fda0a45ee293fccd2bca5c79e
gcfbn <bmekarski@interia.pl>
2021-03-17 15:22:11 +0100

    Added all four algorithms to main. Main prints average waiting time of every algorithm.

--- src/testPackage/Main.java
+++ src/testPackage/Main.java
@@ -1,7 +1,7 @@
 package testPackage;
 
-import algorithmsPackage.Algorithm;
-import algorithmsPackage.NonPreemptiveShortestJobFirst;
+import algorithmsPackage.*;
+import dataPackage.DataGenerator;
 import dataPackage.Process;
 
 import java.io.FileInputStream;
@@ -11,28 +11,152 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String filePath = "./dataSets.ser";
 
-//        System.out.println(DataGenerator.generateDataFile(filePath));
+        System.out.println(DataGenerator.generateDataFile(filePath));
 
-        ArrayList<Process> processes;
+        System.out.println();
 
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))){
-            int length = inputStream.readInt();
+        // FCFS
+        {
+            System.out.println("FCFS algorithm:");
 
-            for (int i=0; i<length; i++){
-                processes = (ArrayList<Process>)inputStream.readObject();
+            ArrayList<Process> processes;
 
-                Algorithm algorithm = new NonPreemptiveShortestJobFirst(processes);
-                System.out.println("waiting time: " + algorithm.getAverageWaitingTime());
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+
+                int length = inputStream.readInt();
+                System.out.println("Number of datasets: " + length);
+
+                double totalWaitingTime = 0.0;
+
+                for (int i = 0; i < length; i++) {
+                    processes = (ArrayList<Process>) inputStream.readObject();
+
+                    Algorithm algorithm = new FirstComeFirstServed(processes);
+                    totalWaitingTime += algorithm.getAverageWaitingTime();
+                }
+
+                double averageWaitingTime = totalWaitingTime / length;
+                System.out.println("Average waiting time: " + averageWaitingTime);
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
             }
-
-
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
         }
 
+        System.out.println();
+
+        // Non-preemptive SJF
+        {
+            System.out.println("Non-preemptive SJF algorithm:");
+
+            ArrayList<Process> processes;
+
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+
+                int length = inputStream.readInt();
+                System.out.println("Number of datasets: " + length);
+
+                double totalWaitingTime = 0.0;
+
+                for (int i = 0; i < length; i++) {
+                    processes = (ArrayList<Process>) inputStream.readObject();
+
+                    Algorithm algorithm = new NonPreemptiveShortestJobFirst(processes);
+                    totalWaitingTime += algorithm.getAverageWaitingTime();
+                }
+
+                double averageWaitingTime = totalWaitingTime / length;
+                System.out.println("Average waiting time: " + averageWaitingTime);
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println();
+
+        // Preemptive SJF
+        {
+            System.out.println("Preemptive SJF algorithm:");
+
+            ArrayList<Process> processes;
+
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+
+                int length = inputStream.readInt();
+                System.out.println("Number of datasets: " + length);
+
+                double totalWaitingTime = 0.0;
+
+                for (int i = 0; i < length; i++) {
+                    processes = (ArrayList<Process>) inputStream.readObject();
+
+                    Algorithm algorithm = new PreemptiveShortestJobFirst(processes);
+                    totalWaitingTime += algorithm.getAverageWaitingTime();
+                }
+
+                double averageWaitingTime = totalWaitingTime / length;
+                System.out.println("Average waiting time: " + averageWaitingTime);
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println();
+
+        // Round Robin
+        {
+            System.out.println("Round Robin algorithm:");
+
+            ArrayList<Process> processes;
+
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+
+                int length = inputStream.readInt();
+                System.out.println("Number of datasets: " + length);
+
+                double totalWaitingTime = 0.0;
+
+                for (int i = 0; i < length; i++) {
+                    processes = (ArrayList<Process>) inputStream.readObject();
+
+                    Algorithm algorithm = new RoundRobin(processes);
+                    totalWaitingTime += algorithm.getAverageWaitingTime();
+                }
+
+                double averageWaitingTime = totalWaitingTime / length;
+                System.out.println("Average waiting time: " + averageWaitingTime);
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+//    Example output:
+//
+//    true
+//
+//    FCFS algorithm:
+//    Number of datasets: 40
+//    Average waiting time: 137.6926975828731
+//
+//    Non-preemptive SJF algorithm:
+//    Number of datasets: 40
+//    Average waiting time: 62.68004133227023
+//
+//    Preemptive SJF algorithm:
+//    Number of datasets: 40
+//    Average waiting time: 60.44126586435813
+//
+//    Round Robin algorithm:
+//    Number of datasets: 40
+//    Average waiting time: 139.56037233097703
+//
+//    Process finished with exit code 0
 }
